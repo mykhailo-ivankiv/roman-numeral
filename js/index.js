9ab480fd3908b4c341a3e77c457ec2932f9b32b4
Mykhailo Ivankiv <neformal.lviv@gmail.com>
2020-07-03 00:47:41 +0200

    Update UI

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import decimalToRoman from './decimalToRoman.js';
-import romanToDecimal from "./romanToDecimal.js";
+import romanToDecimal from './romanToDecimal.js';
 
 const romanInput = document.getElementById('roman');
 const romanErrorEl = document.getElementById('romanError');
@@ -7,47 +7,40 @@ const romanErrorEl = document.getElementById('romanError');
 const arabInput = document.getElementById('arab');
 const arabErrorEl = document.getElementById('arabError');
 
-const processRomanToDec = (romanString) => {
-  romanToDecimal(romanString)
-    .then((value) => {
-      arabInput.value = value;
-      romanErrorEl.getElementsByClassName('roman-app__error-description')[0].innerHTML = '';
-      romanErrorEl.getElementsByClassName('roman-app__error-string')[0].innerHTML = '';
-    })
-    .catch(({ msg, position }) => {
-      romanErrorEl.getElementsByClassName('roman-app__error-description')[0].innerHTML = msg;
+const processRomanToDecimal = async (romanString) => {
+  try {
+    const value = await romanToDecimal(romanString);
 
-      romanErrorEl.getElementsByClassName('roman-app__error-string')[0].innerHTML =
-        romanString.slice(0, position) +
-        `<i class="roman-app__error-letter">${romanString[position]}</i>` +
-        romanString.slice(position + 1);
-    });
+    arabInput.value = value;
+    romanErrorEl.getElementsByClassName('roman-app__error-description')[0].innerHTML = '';
+    romanErrorEl.getElementsByClassName('roman-app__error-string')[0].innerHTML = '';
+  } catch ({ msg, position }) {
+    romanErrorEl.getElementsByClassName('roman-app__error-description')[0].innerHTML = msg;
+    romanErrorEl.getElementsByClassName('roman-app__error-string')[0].innerHTML =
+      romanString.slice(0, position) +
+      `<i class="roman-app__error-letter">${romanString[position]}</i>` +
+      romanString.slice(position + 1);
+  }
 };
 
-const processDecToRoman = (decString) => {
-  decimalToRoman(decString)
-    .then((value) => {
-      romanInput.value = value;
+const processDecimalToRoman = async (decString) => {
+  try {
+    const value = await decimalToRoman(decString);
 
-      arabErrorEl.getElementsByClassName('roman-app__error-description')[0].innerHTML = '';
-      arabErrorEl.getElementsByClassName('roman-app__error-string')[0].innerHTML = '';
-    })
-    .catch(({ msg, position }) => {
-      arabErrorEl.getElementsByClassName('roman-app__error-description')[0].innerHTML = msg;
-      arabErrorEl.getElementsByClassName('roman-app__error-string')[0].innerHTML = '';
-    });
+    romanInput.value = value;
+    arabErrorEl.getElementsByClassName('roman-app__error-description')[0].innerHTML = '';
+    arabErrorEl.getElementsByClassName('roman-app__error-string')[0].innerHTML = '';
+  } catch ({ msg, position }) {
+    arabErrorEl.getElementsByClassName('roman-app__error-description')[0].innerHTML = msg;
+    arabErrorEl.getElementsByClassName('roman-app__error-string')[0].innerHTML = '';
+  }
+};
+const init = () => {
+  if (romanInput.value) processRomanToDecimal(romanInput.value);
+  else if (arabInput.value) processDecimalToRoman(arabInput.value);
+
+  romanInput.addEventListener('input', () => processRomanToDecimal(romanInput.value));
+  arabInput.addEventListener('input', () => processDecimalToRoman(arabInput.value));
 };
 
-if (romanInput.value) {
-  processRomanToDec(romanInput.value);
-} else if (arabInput.value) {
-  processDecToRoman(arabInput.value);
-}
-
-romanInput.addEventListener('input', () => {
-  processRomanToDec(romanInput.value);
-});
-
-arabInput.addEventListener('input', () => {
-  processDecToRoman(arabInput.value);
-});
+init();
